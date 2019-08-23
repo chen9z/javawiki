a3ba0548c3e6be75693041194116d3a1e4db5745
yclooper <yclooper@163.com>
2019-08-23 15:04:17 +0800

    init

--- wiki/src/alg/Demo.java
+++ wiki/src/alg/Demo.java
@@ -10,7 +10,11 @@ public class Demo {
     public static void main(String[] args) {
 
         SortDemo sortDemo = new SortDemo();
-        int[] a = {1, 3, 2, 9, 8, 0, 11, 23, 5};
-        System.out.println(Arrays.toString(sortDemo.quickSort(a)));
+        int[] a = {1, 3, 2, 9, 8,8, 0, 11, 23, 5};
+
+        int left=1;
+        System.out.println(a[left++]);
+        System.out.println(left);
+//        System.out.println(Arrays.toString(sortDemo.quickSort(a)));
     }
 }
--- wiki/src/alg/SortDemo.java
+++ wiki/src/alg/SortDemo.java
@@ -62,39 +62,61 @@ public class SortDemo {
      * @return
      */
     public int[] quickSort(int[] items) {
-        megreSort(items,0 , items.length-1);
+        megreSort(items, 0, items.length - 1);
         return items;
     }
 
-    private int partition(int[] items,int start,int end) {
-        int pivot = items[end];
-        int i=start;
-        for (int j = start; j < end; j++) {
-            if (items[j] < pivot) {
-                if (i == j) {
-                    i++;
-                }else {
-                    int t = items[i];
-                    items[i++] = items[j];
-                    items[j]=t;
-                }
-            }
-        }
-
-        int tmp = items[i];
-        items[i] = items[end];
-        items[end] = tmp;
-        return i;
-    }
+//    private int partition(int[] items,int start,int end) {
+//        int pivot = items[end];
+//        int i=start;
+//        for (int j = start; j < end; j++) {
+//            if (items[j] < pivot) {
+//                if (i == j) {
+//                    i++;
+//                }else {
+//                    int t = items[i];
+//                    items[i++] = items[j];
+//                    items[j]=t;
+//                }
+//            }
+//        }
+//
+//        int tmp = items[i];
+//        items[i] = items[end];
+//        items[end] = tmp;
+//        return i;
+//    }
 
     private void megreSort(int[] items, int start, int end) {
 
-        if (start >=end) {
+        if (start >= end) {
             return;
         }
-        int pivot = partition(items,start,end);
+        int pivot = partIndex(items, start, end);
         megreSort(items, start, pivot - 1);
         megreSort(items, pivot + 1, end);
+    }
+
+
+    private int partIndex(int[] items, int start, int end) {
+        int pivot = items[end];
+        int left = start;
+        for (int i = start; i < end; i++) {
+            if (items[i] < pivot) {
+                if (left == i) {
+                    left++;
+                } else {
+                    int temp = items[left];
+                    items[left++] = items[i];
+                    items[i] = temp;
+                }
+            }
+        }
+        int temp = items[left];
+        items[left] = pivot;
+        items[end] = temp;
+        return left;
+
     }
 
 }
